847b3e791081613301cd135e8452ece7dd01a1b3
Talha Abdulkuddus <30045102+Talha-Ak@users.noreply.github.com>
2024-02-27 13:32:33 +0000

    Adjust options

--- lua/options.lua
+++ lua/options.lua
@@ -2,15 +2,46 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- Sync OS & Vim clipboard
-vim.opt.clipboard = 'unnamedplus'
-
 -- Line Numbers
 vim.opt.number = true
 vim.opt.relativenumber = true
 
+-- Enable mouse mode
+vim.opt.mouse = 'a'
+
+-- Don't show mode in command line
+vim.opt.showmode = false
+
+-- Sync OS & Vim clipboard
+vim.opt.clipboard = 'unnamedplus'
+
+-- Case-insensitive searching UNLESS \C or capital in search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
 -- Always draw signcolumn
 vim.opt.signcolumn = 'yes'
+
+-- Decrease update time
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+
+-- Change default split positions
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+-- Sets how neovim will display certain whitespace in the editor.
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- Preview substitutions live
+vim.opt.inccommand = 'split'
+
+-- Highlight cursor line
+vim.opt.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 10
 
 -- Indentation
 vim.opt.expandtab = true
@@ -18,7 +49,7 @@ vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 
--- Word Wrap
+-- Disable word Wrap
 vim.opt.wrap = false
 
 -- Character column marker
@@ -27,15 +58,8 @@ vim.opt.colorcolumn = '120'
 -- Enable virtual editing for <C-v>
 vim.opt.virtualedit = 'block'
 
--- Change default split positions
-vim.opt.splitbelow = true
-vim.opt.splitright = true
-
 -- Enable 24-bit colour
 vim.opt.termguicolors = true
 
 -- Completion height
 vim.opt.pumheight = 10
-
--- Highlight cursor line
-vim.opt.cursorline = true
