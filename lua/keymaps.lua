ffab19e894783d30b115c9933d49ba9aab7a6e6c
Talha Abdulkuddus <30045102+Talha-Ak@users.noreply.github.com>
2024-02-27 13:32:50 +0000

    Add core keymaps

---
+++ lua/keymaps.lua
@@ -0,0 +1,13 @@
+-- Clear search highlight on pressing <Esc> in normal mode
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+-- Highlight when yanking (copying) text
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end
+})
