76959cbb80d59ce3726a3d01f3f42e92b87cb23c
Talha Abdulkuddus <30045102+Talha-Ak@users.noreply.github.com>
2024-02-28 16:25:28 +0000

    Move treesitter to seperate file

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -9,18 +9,6 @@ return {
         end
     },
     {
-        'nvim-treesitter/nvim-treesitter',
-        build = ':TSUpdate',
-        config = function()
-            require('nvim-treesitter.configs').setup({
-                ensure_installed = { 'c', 'lua', 'vim', 'vimdoc', 'query' },
-                auto_install = true,
-                highlight = { enable = true },
-                indent = { enable = true }
-            })
-        end
-    },
-    {
         'tpope/vim-sleuth',
     }
 }
---
+++ lua/plugins/syntax.lua
@@ -0,0 +1,14 @@
+return {
+    {
+        'nvim-treesitter/nvim-treesitter',
+        build = ':TSUpdate',
+        config = function()
+            require('nvim-treesitter.configs').setup({
+                ensure_installed = { 'c', 'lua', 'vim', 'vimdoc', 'query' },
+                auto_install = true,
+                highlight = { enable = true },
+                indent = { enable = true }
+            })
+        end
+    },
+}
