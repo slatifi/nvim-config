1427f3c913415b62fc20660ff8180f9eceb092de
Saif Latifi <saif.latifi@gmail.com>
2024-03-28 13:22:45 +0000

    Add lazy loading to Jupyter plugins

--- init.lua
+++ init.lua
@@ -1,6 +1,5 @@
 require("options")
 require("keymaps")
-require("jnbcmds")
 
 package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?/init.lua;"
 package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?.lua;"
--- lua/plugins/jupyter.lua
+++ lua/plugins/jupyter.lua
@@ -1,10 +1,64 @@
 -- To use a jupyter notebook, setup a virtual environment with the following packages: jupyter jupytext pynvim
+
+-- automatically import output chunks from a jupyter notebook
+-- tries to find a kernel that matches the kernel in the jupyter notebook
+-- falls back to a kernel that matches the name of the active venv (if any)
+local imb = function(e) -- init molten buffer
+    vim.schedule(function()
+        local kernels = vim.fn.MoltenAvailableKernels()
+        local try_kernel_name = function()
+            local metadata = vim.json.decode(io.open(e.file, "r"):read("a"))["metadata"]
+            return metadata.kernelspec.name
+        end
+        local ok, kernel_name = pcall(try_kernel_name)
+        if not ok or not vim.tbl_contains(kernels, kernel_name) then
+            kernel_name = nil
+            local venv = os.getenv("VIRTUAL_ENV")
+            if venv ~= nil then
+                kernel_name = string.match(venv, "/.+/(.+)")
+            end
+        end
+        if kernel_name ~= nil and vim.tbl_contains(kernels, kernel_name) then
+            vim.cmd(("MoltenInit %s"):format(kernel_name))
+        end
+        vim.cmd("MoltenImportOutput")
+    end)
+end
+
+-- automatically import output chunks from a jupyter notebook
+vim.api.nvim_create_autocmd("BufAdd", {
+    pattern = { "*.ipynb" },
+    callback = imb,
+})
+
+-- we have to do this as well so that we catch files opened like nvim ./hi.ipynb
+vim.api.nvim_create_autocmd("BufEnter", {
+    pattern = { "*.ipynb" },
+    callback = function(e)
+        if vim.api.nvim_get_vvar("vim_did_enter") ~= 1 then
+            imb(e)
+        end
+    end,
+})
+
+-- automatically export output chunks to a jupyter notebook on write
+vim.api.nvim_create_autocmd("BufWritePost", {
+    pattern = { "*.ipynb" },
+    callback = function()
+        if require("molten.status").initialized() == "Molten" then
+            vim.cmd("MoltenExportOutput!")
+        end
+    end,
+})
+
+
 return {
     {
         "benlubas/molten-nvim",
         version = "^1.0.0", -- use version <2.0.0 to avoid breaking changes
         dependencies = { "3rd/image.nvim" },
         build = ":UpdateRemotePlugins",
+        ft = {"quarto", "markdown"},
         init = function()
             -- these are examples, not defaults. Please see the readme
             vim.g.molten_image_provider = "image.nvim"
@@ -22,6 +76,7 @@ return {
     {
         -- see the image.nvim readme for more information about configuring this plugin
         "3rd/image.nvim",
+        ft = {"quarto", "markdown"},
         opts = {
             backend = "kitty", -- whatever backend you would like to use
             max_width = 100,
@@ -68,6 +123,7 @@ return {
     },
     {
         "jmbuhr/otter.nvim",
+        ft = {"quarto", "markdown"},
     },
     {
         "GCBallesteros/jupytext.nvim",
