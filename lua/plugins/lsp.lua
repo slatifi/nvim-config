642589f5e15c0a871ba43b78e97d04eaafd140d2
Saif Latifi <saif.latifi@gmail.com>
2025-03-31 16:30:02 +0100

    Add markdown preview plugin

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -99,7 +99,7 @@ return {
 
             local ensure_installed = vim.tbl_keys(servers or {})
             vim.list_extend(ensure_installed, {
-                "stylua", -- Used to format lua code
+                "stylua",   -- Used to format lua code
                 "autopep8", -- Used to format python code
             })
             require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
