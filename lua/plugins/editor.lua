edf7b9830a60aec33c7e23f39478557e01f23817
Saif Latifi <saif.latifi@gmail.com>
2025-05-19 22:55:05 +0100

    Feat: Add harpoon for quick switching

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -12,6 +12,22 @@ return {
         end
     },
     {
+        "ThePrimeagen/harpoon",
+        branch = "harpoon2",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            local harpoon = require("harpoon")
+            harpoon.setup()
+
+            -- Keymaps
+            vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+            vim.keymap.set("n", "<leader>s", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+            vim.keymap.set("n", "<C-a>", function() harpoon:list():prev() end)
+            vim.keymap.set("n", "<C-s>", function() harpoon:list():next() end)
+        end,
+
+    },
+    {
         "folke/todo-comments.nvim",
         cmd = { "TodoTrouble", "TodoTelescope" },
         event = { "BufReadPost", "BufWritePost", "BufNewFile" },
