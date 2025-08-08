253487c3e7a95a3e3fe19b1f95c743e0cb279805
Saif Latifi <saif.latifi@gmail.com>
2025-08-08 20:08:29 +0100

    Feat: Add copilot chat

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -3,9 +3,10 @@ return {
         "nvim-telescope/telescope.nvim",
         tag = "0.1.5",
         event = "VimEnter",
-        dependencies = { "nvim-lua/plenary.nvim" },
+        dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope-ui-select.nvim" },
         config = function()
             local builtin = require("telescope.builtin")
+            require("telescope").load_extension("ui-select")
             vim.keymap.set("n", "<C-p>", builtin.git_files, {})
             vim.keymap.set("n", "<leader>sf", builtin.find_files, {})
             vim.keymap.set("n", "<leader>sg", builtin.live_grep, {})
@@ -41,10 +42,6 @@ return {
     },
     {
         "tpope/vim-sleuth",
-    },
-    {
-        "github/copilot.vim",
-        event = "InsertEnter",
     },
     {
         "numToStr/Comment.nvim",
